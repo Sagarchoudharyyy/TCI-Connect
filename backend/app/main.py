609bb4418a5068d34b286553a3998f87721752f0
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-05-25 16:24:13 +0530

    completed login page

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,21 @@ from fastapi import FastAPI
 from app.database.database import engine,Base
 from app.models.user_model import User
 from app.api.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="TCI Connect API",
     description="Dental Lab Management System",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 Base.metadata.create_all(bind=engine)
