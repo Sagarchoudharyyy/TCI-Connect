0f06c8e330c9ca78392a764efca200529dbaed4a
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-05-27 16:44:30 +0530

    little change in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,6 +25,7 @@ app.add_middleware(
 Base.metadata.create_all(bind=engine)
 app.include_router(auth_router)
 app.include_router(doctor_router)
+app.include_router(case_router)
 
 app.include_router(
     case_router,
