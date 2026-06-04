d2da7cd7dd6568c0ec049db69d6bbd4460b77939
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-06-04 18:05:22 +0530

    doctor header and sidebar frontend

---
+++ frontend/src/Doctor/Cases.jsx
@@ -0,0 +1,18 @@
+import DoctorSideBar from "../components/DoctorSideBar";
+import DoctorHeader from "../components/DoctorHeader";
+import "../DoctorStyle/doctor-dashboard.css";
+
+function DoctorCases() {
+    return (
+        <div className="container-fluid p-0">
+            <div className="row g-0 doctor-dashboard-main">
+                <DoctorSideBar />
+                <div className="col-md-9 doctor-main-content">
+
+                    <DoctorHeader title="Dashboard" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default DoctorCases;
