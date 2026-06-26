de10135330b1e60439b3b68bcdafb4ab4a8752d6
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-06-26 14:46:53 +0530

    all done in home ui

--- frontend/src/design-part/html/SubmitCase.jsx
+++ frontend/src/design-part/html/SubmitCase.jsx
@@ -22,19 +22,19 @@ function SubmitCase() {
     return (
         <>
             <Header />
-            <section class="all-banner">
-                <div class="container h-100">
-                    <div class="row justify-content-center align-items-center h-100">
-                        <div class="col-lg-11">
-                            <div class="all-banner-meta">
-                                <div class="all-banner-content">
-                                    <ul class="banner-nav">
-                                        <li><a href="https://mediumseagreen-herring-541085.hostingersite.com/">Home</a></li>
-                                        <li><a href="" class="active">Submit a Case</a></li>
+            <section className="all-banner">
+                <div className="container h-100">
+                    <div className="row justify-content-center align-items-center h-100">
+                        <div className="col-lg-11">
+                            <div className="all-banner-meta">
+                                <div className="all-banner-content">
+                                    <ul className="banner-nav">
+                                        <li><Link to="/">Home</Link></li>
+                                        <li><Link to="/submit-case" className="active">Submit A Case</Link></li>
                                     </ul>
-                                    <h1>Submit a Case</h1>
+                                    <h1>Submit Link Case</h1>
                                 </div>
-                                <div class="all-banner-img">
+                                <div className="all-banner-img">
                                     <img src={casebanne} alt="" />
                                 </div>
 
@@ -43,25 +43,25 @@ function SubmitCase() {
                     </div>
                 </div>
             </section>
-            <section class="submit-case">
-                <div class="container pb-5">
-                    <div class="row justify-content-center">
-                        <div class="col-lg-11">
-                            <div class="submit-case-meta">
+            <section className="submit-case">
+                <div className="container pb-5">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-11">
+                            <div className="submit-case-meta">
 
-                                <a href="https://mediumseagreen-herring-541085.hostingersite.com/client/submit-case" class="submit-case-single">
-                                    <div class="submit-case-img">
+                                <Link to="/submit-case" className="submit-case-single">
+                                    <div className="submit-case-img">
                                         <img src={submitcase} alt="" />
                                     </div>
-                                    <div class="submit-case-content">
+                                    <div className="submit-case-content">
                                         <h4>Send your Scanned Files</h4>
                                         <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M1 8H15" stroke="#3D9BFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"></path>
-                                            <path d="M8 1L15 8L8 15" stroke="#3D9BFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"></path>
+                                            <path d="M1 8H15" stroke="#3D9BFF" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path>
+                                            <path d="M8 1L15 8L8 15" stroke="#3D9BFF" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round"></path>
                                         </svg>
                                         </span>
                                     </div>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
