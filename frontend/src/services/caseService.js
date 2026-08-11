cc980c98496690177ff376e7f820aa465c8f38fa
Sagarchoudharyyy <sagarchoudhary7262@gmail.com>
2026-08-11 11:00:44 +0530

    new thing add for production

--- frontend/src/services/caseService.js
+++ frontend/src/services/caseService.js
@@ -1,34 +1,42 @@
 import api from "./api";
 
+
 export const getCases = (params) => {
   return api.get("/cases", {
     params,
   });
 };
 
+
 export const getCaseById = (id) => {
   return api.get(`/cases/${id}`);
 };
+
 
 export const createCase = (data) => {
   return api.post("/cases", data);
 };
 
+
 export const updateCase = (id, data) => {
   return api.put(`/cases/${id}`, data);
 };
+
 
 export const deleteCase = (id) => {
   return api.delete(`/cases/${id}`);
 };
 
+
 export const uploadCaseFile = (caseId, formData) => {
   return api.post(`/cases/${caseId}/upload`, formData);
 };
 
+
 export const getCaseFiles = () => {
   return api.get("/case_files");
 };
+
 
 export const updatePreviewStatus = (caseId, preview_status) => {
   return api.put(`/cases/${caseId}/preview-status`, {
